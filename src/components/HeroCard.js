83e3b66e2a130fe3a0786498c0b090b14d9a946a
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-02 19:20:31 +0530

    Added img file

---
+++ src/components/HeroCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroCard = () => {
+    return (
+        <div className="m-2 h-32 w-48 bg-gray-200 flex flex-col items-center">
+            <div className="flex justify-between">
+                <div className="p-5">GOOGL</div>
+                <img className="h-16 p-5" src="./GOOGL.png" alt="" />
+            </div>
+            <div className="flex items-center">
+                <h1 className="text-3xl">1515 USD</h1>
+            </div>
+        </div>
+    )
+}
+
+export default HeroCard
