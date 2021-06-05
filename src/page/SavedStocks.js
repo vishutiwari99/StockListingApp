b1d8d5bc64df01debc67706ae2fe04a6025279ca
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-05 12:50:47 +0530

    some changes

--- src/page/SavedStocks.js
+++ src/page/SavedStocks.js
@@ -10,7 +10,7 @@ const SavedStocks = () => {
     const history = useHistory();
 
     const deleteStocksHandler = async (id) => {
-        await axios.delete(`${URL}${id}`);
+        await axios.delete(`api/${id}`);
         setReload(!reload)
     }
     useEffect(() => {
